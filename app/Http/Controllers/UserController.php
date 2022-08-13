14d291c82cf564b1ddc3846192e0307893be1998
parvazreza00 <93507402+parvazreza00@users.noreply.github.com>
2022-08-13 18:38:53 +0600

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function register(Request $request){
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required',
             'password' => 'required|min:8',
         ]);
+        if($validator->fails()){
+            return response()->json(['status' => 'Fails', 'validation_errors' => $validator->errors()]);
+        }
+
+        $data = $request->all();
+        $data['password'] = Hash::make($request->password);
+
+        $user = User::create($data);
+        if($user){
+            return response()->json(['status' => 'success', 'message' => 'User registration created successfully', 'data' => $user]);
+        }else{
+            return response()->json(['status' => 'Fail', 'message' => 'User registration fails']);
+        }
     }
 }
